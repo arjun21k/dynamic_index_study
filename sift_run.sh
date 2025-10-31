83d38f1561e02ead3e09282a82a938d70458c173
Arjun Kashyap <32374861+arjun21k@users.noreply.github.com>
2025-10-31 16:31:02 -0700

    Update sift_run.sh

--- sift_run.sh
+++ sift_run.sh
@@ -1,23 +1,21 @@
-DATA_BIN=/localscratch/arjun/freshdiskann/sift1M/sift_base.fbin
-QUERY_BIN=/localscratch/arjun/freshdiskann/sift1M/sift_query.fbin
-#GT_FILE=/localscratch/arjun/freshdiskann/sift1M/sift_query_base_gt5
-GT_FILE=/localscratch/arjun/freshdiskann/sift1M/sift100k/sift_base_100k_query_gt5
+DATA_BIN=<path to data bin>
+QUERY_BIN=<path to query bin>
+GT_FILE=<path to ground truth file>
 NUM_DEL=100000
 NUM_INS=100000
 NUM_ITERS=10
 MERGE_THREADS=20
-#LOG_FILE=/home/arjun/vectorDB/freshdiskann/DiskANN/logs/node07/sift1M_streaming_merge_${NUM_DEL}_del_${NUM_INS}_ins_iter_${NUM_ITERS}_merge_20
-LOG_FILE=/home/arjun/vectorDB/freshdiskann/DiskANN/logs/steady_state_800k_concurr_merge_insert_${NUM_DEL}_del_${NUM_INS}_ins_iter_${NUM_ITERS}_merge_${MERGE_THREADS}
+LOG_FILE=<path to log file>
 
-#WORKING_PATH=/localscratch/arjun/freshdiskann/sift1M/test_concurr_merge_insert_${NUM_DEL}_del_${NUM_INS}_ins_iter_${NUM_ITERS}
-WORKING_PATH=/localscratch/arjun/freshdiskann/sift1M/steady_state_800k_concurr_merge_insert_${NUM_DEL}_del_${NUM_INS}_ins_iter_${NUM_ITERS}_merge_${MERGE_THREADS}
+WORKING_PATH=<path where initial index resides on SSD>
 
-BASE_PREFIX=${WORKING_PATH}/initial_index_100k_r64_l75_pq_32
+BASE_PREFIX=${WORKING_PATH}/<indec name>
 MEM_PREFIX=${WORKING_PATH}/mem_prefix
 MERGE_PREFIX=${WORKING_PATH}/merge_prefix
 
-BIN_PATH=/home/arjun/vectorDB/freshdiskann/DiskANN/build/tests
+BIN_PATH=build/tests
 
+# Values used from FreshDiskANN system
 L_MEM=75
 ALPHA_MEM=1.2
 L_DISK=75
